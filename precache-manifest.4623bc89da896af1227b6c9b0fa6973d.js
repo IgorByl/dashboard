db7affa9979cc83bf0fb4a205b08b9330d49dc98
IgorByl <i.bylatu@mail.ru>
2019-03-01 14:58:07 +0200

    Updates

---
+++ precache-manifest.4623bc89da896af1227b6c9b0fa6973d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa4cf97a89d4c21ecfec",
+    "url": "/dashboard/static/js/runtime~main.fa4cf97a.js"
+  },
+  {
+    "revision": "502b50015013eccd2bfb",
+    "url": "/dashboard/static/js/main.502b5001.chunk.js"
+  },
+  {
+    "revision": "2e3a38a04c8e4b9604e0",
+    "url": "/dashboard/static/js/2.2e3a38a0.chunk.js"
+  },
+  {
+    "revision": "502b50015013eccd2bfb",
+    "url": "/dashboard/static/css/main.98a4986d.chunk.css"
+  },
+  {
+    "revision": "d5fae12b69bc5ae1f7d578023773026c",
+    "url": "/dashboard/index.html"
+  }
+];
